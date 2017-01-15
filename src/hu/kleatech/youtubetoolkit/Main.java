4d8d6ad80250750a55c997426d13abd91d56c049
KleaTech <>
2017-01-15 19:31:33 +0100

    fix Shell integration

--- src/hu/kleatech/youtubetoolkit/Main.java
+++ src/hu/kleatech/youtubetoolkit/Main.java
@@ -1,13 +1,19 @@
+//author: Ádám Bozzay
+//Build: development only
+
 package hu.kleatech.youtubetoolkit;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -29,14 +35,16 @@ public class Main extends Application{
 		layout.setPadding(new Insets(10));
 
 		TextArea input = new TextArea("dir");
+		    input.setFont(Font.font("Consolas"));
 		layout.setBottom(input);
 
 		TextArea output = new TextArea();
-		output.setEditable(false);
+		    output.setEditable(false);
+			output.setFont(Font.font("Consolas"));
 		layout.setCenter(output);
 
 		button = new Button("OK");
-		button.setOnAction(e -> output.setText(executeCommand("cmd /c " + input.getText())));
+		button.setOnAction(e -> executeCommand("cmd /c" + input.getText(), output));
 		layout.setRight(button);
 
 		Scene scene = new Scene(layout, 640, 480);
@@ -44,19 +52,35 @@ public class Main extends Application{
 		window.show();
 	}
 
-	@SuppressWarnings({"NestedAssignment", "StringConcatenationInsideStringBufferAppend", "StringBufferWithoutInitialCapacity"})
-	private String executeCommand(String command) {
-		StringBuilder output = new StringBuilder();
-		Process p;
-		try {
-			p = Runtime.getRuntime().exec(command);
-			p.waitFor();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line;
-			while ((line = reader.readLine())!= null) { output.append(line + "\n"); }
-		} catch (Exception e) {
-			return "Error: " + e.toString();
+	@SuppressWarnings("NestedAssignment")
+	private void executeCommand(String command, TextArea output) {
+		Thread thread = new Thread(() -> {
+		    updateTextArea("", output, false);
+		    Process p;
+		   try {
+			    p = Runtime.getRuntime().exec(command);
+			    p.waitFor(1, TimeUnit.SECONDS);
+			    System.err.println("Line reached");
+			    BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream(), "UTF-8"));
+    			String line;
+	    		while ((line = reader.readLine())!= null) { updateTextArea(line + "\n", output, true); }
+		    } catch (Exception e) {
+			    System.err.println("Error: " + e.toString());
+		    }
+		});
+		thread.start();
+	}
+
+	private void updateTextArea(String text, TextArea outputTextArea, boolean append) {
+		if (Platform.isFxApplicationThread()) {
+			if (append) { outputTextArea.appendText(text); }
+			else { outputTextArea.setText(text); }
 		}
-		return output.toString();
+		else {
+			Platform.runLater(() -> {
+		        if (append) { outputTextArea.appendText(text); }
+			    else { outputTextArea.setText(text); }}
+			);
+		}
 	}
 }
